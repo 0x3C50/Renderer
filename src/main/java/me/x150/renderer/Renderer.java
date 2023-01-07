5d1a0d7f4db8da5effcc43b50344d8f095528401
0x3C50 <99053360+0x3C50@users.noreply.github.com>
2023-01-07 17:57:57 +0100

    Initial commit

---
+++ src/main/java/me/x150/renderer/Renderer.java
@@ -0,0 +1,10 @@
+package me.x150.renderer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Renderer implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/x150/renderer/client/RendererClient.java
@@ -0,0 +1,13 @@
+package me.x150.renderer.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class RendererClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
